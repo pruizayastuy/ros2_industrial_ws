3649802a4bad7df5ed7c7c0df6fe3153a6747a51
Tu Nombre <tucorreo@example.com>
2025-06-12 20:43:13 +0200

    .

--- install/industrial_turtle/share/industrial_turtle/package.ps1
+++ install/industrial_turtle/share/industrial_turtle/package.ps1
@@ -112,5 +112,6 @@ $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.Ful
 
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/industrial_turtle/hook/pythonpath.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/industrial_turtle/hook/ament_prefix_path.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\../../build/industrial_turtle/share/industrial_turtle/hook/pythonpath_develop.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
